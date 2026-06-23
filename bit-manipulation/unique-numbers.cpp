b4f7d169ec2befb47c30941bd3fddf81f5afac1f
Kushal Shrestha <whotfisksl.17@gmail.com>
2026-06-23 00:12:44 -0500

    unique numbers

---
+++ bit-manipulation/unique-numbers.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#define int long long
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define ps(x, y) fixed << setprecision(y) << x
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+void i_c_p_c()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int32_t main()
+{
+    i_c_p_c();
+
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> nums[i];
+
+    int ans = nums[0];
+    for (int i = 1; i < n; i++)
+    {
+        ans = ans ^ nums[i];
+    }
+
+    cout << ans << '\n';
+
+    return 0;
+}

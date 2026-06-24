836ce5e570de9fe24cb62dbe13388edc7833c185
Kushal Shrestha <whotfisksl.17@gmail.com>
2026-06-23 21:24:30 -0500

    clear and update bits

--- bit-manipulation/ith-bit.cpp
+++ bit-manipulation/ith-bit.cpp
@@ -1,23 +1,61 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
+int get_bit(int n, int i)
+{
+    int mask = 1 << i;
+
+    return (n & mask);
+}
+
+void set_bit(int &n, int i)
+{
+    int mask = 1 << i;
+
+    n = (n | mask);
+}
+
+void clear_bit(int &n, int i)
+{
+    int mask = 1 << i;
+    mask = ~mask;
+
+    n = n & mask;
+}
+
+void update_bit(int &n, int i, int v)
+{
+    int mask = 1 << i;
+
+    if (v == 0)
+    {
+        mask = ~mask;
+        n = n & mask;
+    }
+
+    else
+        n = n | mask;
+}
+
 int main()
 {
-    int num;
-    cin >> num;
+    cout << get_bit(10, 1) << '\n';
 
-    int i;
-    cin >> i;
+    int n = 10;
 
-    int ith = 1 << i - 1;
+    set_bit(n, 2);
 
-    cout << (num & ith) << '\n';
+    cout << n << '\n';
 
-    num = (num | (1 << 2));
+    clear_bit(n, 2);
 
-    cout << num << '\n';
+    cout << n << '\n';
+
+    update_bit(n, 0, 1);
+    update_bit(n, 3, 0);
+
+    cout << n << '\n';
 
     return 0;
 }

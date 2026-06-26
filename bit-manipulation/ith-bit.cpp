2a2235bf0ecc2281572a665e40b6ca474dfa332e
Kushal Shrestha <whotfisksl.17@gmail.com>
2026-06-26 00:09:49 -0500

    clear range of bits

--- bit-manipulation/ith-bit.cpp
+++ bit-manipulation/ith-bit.cpp
@@ -38,6 +38,21 @@ void update_bit(int &n, int i, int v)
         n = n | mask;
 }
 
+void clear_range(int &n, int i)
+{
+    int mask = (~0) << (i + 1);
+
+    n = n & mask;
+}
+
+void clear_from_i_to_j(int &n, int i, int j)
+{
+    int ith = (1 << i) - 1;
+    int mask = ((~0) << (j + 1)) | ith;
+
+    n = n & mask;
+}
+
 int main()
 {
     cout << get_bit(10, 1) << '\n';
@@ -56,6 +71,16 @@ int main()
     update_bit(n, 3, 0);
 
     cout << n << '\n';
+
+    int m = 31;
+    clear_range(m, 2);
+
+    cout << m << '\n';
+
+    m = 63;
+    clear_from_i_to_j(m, 1, 3);
+
+    cout << m << '\n';
 
     return 0;
 }

8e2a57fe6ffc818499f4b7d93a53b58633f0859e
Kushal Shrestha <whotfisksl.17@gmail.com>
2026-09-15 23:20:41 -0500

    linear search using recursion

---
+++ recursion/linear_search.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int search(vector<int> &arr, int key, int n, int i)
+{
+    if (arr[i] == key)
+        return i;
+
+    if (n == 0)
+        return -1;
+
+    return search(arr, key, n - 1, i + 1);
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 7, 4, 5, 7, 10};
+    int key = 7;
+    int n = arr.size();
+
+    int ans = search(arr, key, n, 0);
+
+    cout << ans;
+    return 0;
+}
